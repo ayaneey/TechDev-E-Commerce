7ad273c0b426e6c3ab6141f57dd685b93f35731f
ayaneey <ayan.h18@hotmail.com>
2022-10-25 08:49:07 +0100

    set up category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,20 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
-// structuring the table 
+// structuring the table
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
