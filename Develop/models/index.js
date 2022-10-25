994e88d4003472cbca5dce9d7aade2e083f46a4f
ayaneey <ayan.h18@hotmail.com>
2022-10-25 08:54:02 +0100

    completed structure of index.js in models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,18 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const seedProductTags = require("../seeds/product-tag-seeds");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, { foreignKey: "category_id" });
 // Categories have many Products
-
+Category.hasMany(Product, { foreignKey: "category_id" });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, { foreignKey: "tag_id" });
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(Product, { foreignKey: "tag_id" });
 
 module.exports = {
   Product,
